a5f3a6c0d6005592c015816791137afdb14ab691
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2026-02-16 14:10:34 +0530

    feat: add Hero section and Button component for enhanced homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import Hero from "@/components/sections/Hero";
+
 
 export default function Home() {
-  
+  return (
+    <main className="min-h-screen">
+      <Hero />
+    </main>
+  );
 }
+
